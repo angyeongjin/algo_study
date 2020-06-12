bbc217e5953f9a40d504c0c4380e4933e4a75e50
glassesjin <38306352+angyeongjin@users.noreply.github.com>
2020-06-13 01:25:13 +0900

    [add] 다트 게임 - 2018 카카오 블라인드 문제

---
+++ PROGRAMMERS/src/level1/다트게임.java
@@ -0,0 +1,47 @@
+public class 다트_게임 {
+
+	public static void main(String[] args) {
+		String dartResult = "1S*2T*3S";
+		System.out.println(solution(dartResult));
+	}
+
+	static int solution(String dartResult) {
+		int answer = 0;
+		char[] command = dartResult.toCharArray();
+		int[] score = new int[3];
+		int idx = -1;
+		for (int i = 0; i < command.length; i++) {
+			
+			// 숫자가 들어오는 경우
+			if (Character.isDigit(command[i])) {
+				idx++;
+				if (command[i+1] == '0') {
+					score[idx] = 10;
+					i++;
+				}
+				else score[idx] = command[i] - '0';
+			}
+			
+			// 보너스가 들어오는 경우
+			if (command[i] == 'D') {
+				score[idx] = score[idx] * score[idx];
+			} else if (command[i] == 'T') {
+				score[idx] = score[idx] * score[idx] * score[idx];
+			}
+			
+			// 옵션이 들어오는 경우
+			if (command[i] == '*') {
+				score[idx] *= 2;
+				if (idx > 0) score[idx-1] *= 2;
+			} else if (command[i] == '#') {
+				score[idx] = -score[idx];
+			}
+		}
+		
+		for (int i = 0; i < score.length; i++) {
+			answer += score[i];
+		}
+		return answer;
+	}
+
+}
